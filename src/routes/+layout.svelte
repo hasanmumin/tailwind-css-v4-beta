9cd22507299637a6ef4c3c1d0128d1e5fa27af95
hasanmumin <0hasanmumin@gmail.com>
2024-11-25 22:37:33 +0300

    Tailwind CSS v4.0 Beta configurations

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="container max-w-screen-2xl p-2">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-4xl text-white">Welcome to SvelteKit</h1>
+<p class="text-2xl text-white">
+	Visit <a class="text-red-500" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
+<p class="text-amber-500">Text-1</p>
+<p class="text-blue-500">Text-2</p>
+<button class="text-2xl text-white hover:bg-amber-500 cursor-grab">Button</button>
